e13fa00d508ef31bc56fd6b49a580738f32f30be
Mahmoud Omara <mahmoud.omara909@gmail.com>
2025-01-23 00:45:29 +0200

    Initial Commit

---
+++ GameGiveaways/GameGiveawaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameGiveawaysApp.swift
+//  GameGiveaways
+//
+//  Created by mac on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameGiveawaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
