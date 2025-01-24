811bc8885c937ae2b2fe824d8236ff5cd50d3192
Mahmoud Omara <mahmoud.omara909@gmail.com>
2025-01-24 12:18:47 +0200

    [Feature] Add HomeViewFactory for managing dependency injection

---
+++ GameGiveaways/Features/Home/HomeViewFactory.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewFactory.swift
+//  GameGiveaways
+//
+//  Created by mac on 24/01/2025.
+//
+
+import SwiftUI
+
+struct HomeViewFactory {
+    static func createHomeView() -> some View {
+        let apiClient = ApiClient()
+        
+        let userLocalDataSource = UserLocalDataSource()
+        let platformsLocalDataSource = PlatformsLocalDataSource()
+        let giveawaysRemoteDataSource = GiveawaysRemoteDataSource(apiClient: apiClient)
+        
+        let userRepository = UserRepository(dataSource: userLocalDataSource)
+        let platformRepository = PlatformRepository(dataSource: platformsLocalDataSource)
+        let giveawaysRepository = GiveawaysRepository(dataSource: giveawaysRemoteDataSource)
+        
+        let getUserProfileUseCase = GetUserProfileUseCase(repository: userRepository)
+        let getPlatformsUseCase = GetPlatformsUseCase(repository: platformRepository)
+        let getAllGiveawaysUseCase = GetAllGiveawaysUseCase(repository: giveawaysRepository)
+        let getGiveawaysByPlatformUseCase = GetGiveawaysByPlatformUseCase(repository: giveawaysRepository)
+        let searchGiveawaysUseCase = SearchGiveawaysUseCase()
+        
+        let viewModel = HomeViewModel(
+            getUserProfileUseCase: getUserProfileUseCase,
+            getPlatformsUseCase: getPlatformsUseCase,
+            getAllGiveawaysUseCase: getAllGiveawaysUseCase,
+            getFilteredGiveawaysUseCase: getGiveawaysByPlatformUseCase,
+            searchGiveawaysUseCase: searchGiveawaysUseCase)
+        
+        let view = HomeView(viewModel: viewModel)
+        return view
+    }
+}
