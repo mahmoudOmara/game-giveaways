21e9f1a2842723b0ac804b04ca52edded67cd8c8
Mahmoud Omara <mahmoud.omara909@gmail.com>
2025-01-24 20:33:44 +0200

    [Feature] Add Giveaway Details ViewModel

---
+++ GameGiveaways/Features/GiveawayDetails/Presentation/ViewModels/GiveawayDetailsViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  GiveawayDetailsViewModel.swift
+//  GameGiveaways
+//
+//  Created by mac on 24/01/2025.
+//
+
+import SwiftUI
+import Combine
+
+enum GiveawayDetailsState {
+    case idle
+    case loading
+    case success(GiveawayDetailEntity)
+    case failure(String)
+}
+
+class GiveawayDetailsViewModel: ObservableObject {
+    // MARK: - Published Properties
+    @Published var state: GiveawayDetailsState = .idle
+
+    // MARK: - Dependencies
+    private let getGiveawayDetailsUseCase: GetGiveawayDetailsUseCaseProtocol
+    private let giveawayID: Int
+    
+    private var cancellables = Set<AnyCancellable>()
+
+    // MARK: - Initializer
+    init(giveawayID: Int, getGiveawayDetailsUseCase: GetGiveawayDetailsUseCaseProtocol) {
+        self.giveawayID = giveawayID
+        self.getGiveawayDetailsUseCase = getGiveawayDetailsUseCase
+        fetchGiveawayDetails()
+    }
+
+    // MARK: - Public Methods
+    func fetchGiveawayDetails() {
+        state = .loading
+        getGiveawayDetailsUseCase.execute(giveawayID: giveawayID)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .failure(let error):
+                    self?.state = .failure(error.localizedDescription)
+                    
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] details in
+                self?.state = .success(details)
+            })
+            .store(in: &cancellables)
+    }
+}
