194cf7b2edeba134e45c853d429011a66cd322b9
Mahmoud Omara <mahmoud.omara909@gmail.com>
2025-01-25 15:20:33 +0200

    [Feature] Implemented GetMoreGiveawaysUseCase

---
+++ GameGiveaways/Features/MorePlatforms/Domain/UseCases/GetMoreGiveawaysUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  GetMoreGiveawaysUseCase.swift
+//  GameGiveaways
+//
+//  Created by mac on 25/01/2025.
+//
+
+
+import Combine
+
+protocol GetMoreGiveawaysUseCaseProtocol {
+    func execute(platforms: [String]) -> AnyPublisher<[String: [GiveawayEntity]], Error>
+}
+
+class GetMoreGiveawaysUseCase: GetMoreGiveawaysUseCaseProtocol {
+    private let getGiveawaysByPlatformUseCase: GetGiveawaysByPlatformUseCaseProtocol
+
+    init(getGiveawaysByPlatformUseCase: GetGiveawaysByPlatformUseCaseProtocol) {
+        self.getGiveawaysByPlatformUseCase = getGiveawaysByPlatformUseCase
+    }
+
+    func execute(platforms: [String]) -> AnyPublisher<[String: [GiveawayEntity]], Error> {
+        let publishers = platforms.map { platform in
+            getGiveawaysByPlatformUseCase.execute(platform: platform)
+                .map { (platform, $0) }
+                .catch { _ in Just((platform, [])).setFailureType(to: Error.self)  }
+        }
+
+        return Publishers.MergeMany(publishers)
+            .collect()
+            .map { Dictionary(uniqueKeysWithValues: $0) }
+            .eraseToAnyPublisher()
+    }
+}
